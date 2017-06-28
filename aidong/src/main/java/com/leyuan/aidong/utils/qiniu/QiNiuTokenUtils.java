7d1e3e1e58d56fa3df3621e1a513a86723f49461
song <523931340@qq.com>
2017-06-28 14:44:03 +0800

    change qiniu bucket name

--- aidong/src/main/java/com/leyuan/aidong/utils/qiniu/QiNiuTokenUtils.java
+++ aidong/src/main/java/com/leyuan/aidong/utils/qiniu/QiNiuTokenUtils.java
@@ -10,7 +10,7 @@ import com.leyuan.aidong.utils.qiniu.token.Auth;
 public class QiNiuTokenUtils {
     public static String ACCESS_KEY = "SOMBFhoOmUgnKcmLYPoYlZxUa4E7E5sJp7nW5YDU";
     public static String SECRET_KEY = "be2JUix4G-CayPHwXa6Wz4TD9E13Ffjc6QQH9qzn";
-    public static String bucketName = "aidong";                        //要上传的空间
+    public static String bucketName = "adong";                        //要上传的空间
 
     public static String getQiNiuToken(){
         Auth auth = Auth.create(ACCESS_KEY, SECRET_KEY);    //密钥配置
