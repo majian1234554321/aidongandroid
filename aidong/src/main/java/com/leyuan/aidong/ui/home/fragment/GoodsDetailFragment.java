2e0a3b0ceecfa28f41170125a52c99e850553812
huyouni110 <63049561@qq.com>
2017-07-03 18:29:53 +0800

    modify videodetail tag show

--- aidong/src/main/java/com/leyuan/aidong/ui/home/fragment/GoodsDetailFragment.java
+++ aidong/src/main/java/com/leyuan/aidong/ui/home/fragment/GoodsDetailFragment.java
@@ -32,53 +32,10 @@ public class GoodsDetailFragment extends BaseFragment {
 
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
-//        tvContent = (TextView) view.findViewById(R.id.tv_content);
         webView = (RichWebView) view.findViewById(R.id.web_view);
 
         if (!TextUtils.isEmpty(content)) {
             Logger.i("RichText", " body = " + content);
-//            ((XRichText) view.findViewById(R.id.richText)).text(content);
-//            tvContent.setText(Html.fromHtml(content, new Html.ImageGetter() {
-//                @Override
-//                public Drawable getDrawable(String source) {
-//                    final Drawable[] drawable = {null};
-//                    Glide.with(GoodsDetailFragment.this)
-//                            .load(source)
-//                            .asBitmap()
-//                            .into(new SimpleTarget<Bitmap>() {
-//                                @Override
-//                                public void onResourceReady(Bitmap resource, GlideAnimation<? super Bitmap> glideAnimation) {
-//                                    drawable[0] = new BitmapDrawable(getActivity().getResources(),resource);
-//                                }
-//                            });
-
-//                    return drawable[0];
-//                }
-//            }, null).toString());
-//            WebSettings settings = webView.getSettings();
-//            settings.setJavaScriptEnabled(true);
-//            webView.setWebViewClient(new WebViewClient(){
-//                @Override
-//                public void onPageFinished(WebView view, String url) {
-//                    super.onPageFinished(view, url);
-//                    webView.loadUrl("javascript:(function(){"
-//                            + "var objs = document.getElementsByTagName('img'); "
-//                            + "for(var i=0;i<objs.length;i++)  " + "{"
-//                            + "var img = objs[i];   "
-//                            + "    img.style.width = '100%';   "
-//                            + "    img.style.height = 'auto';   "
-//                            + "}" + "})()");
-//
-//                }
-//                @Override
-//                public boolean shouldOverrideUrlLoading(WebView view, String url) {
-//                    view.loadUrl(url);
-//                    return true;
-//                }
-//
-//            });
-//            webView.loadDataWithBaseURL(null, content, "text/html", "UTF-8", null);
-
             webView.setRichText(content);
 //            RichText.from(content)
 //                    .autoFix(true)
