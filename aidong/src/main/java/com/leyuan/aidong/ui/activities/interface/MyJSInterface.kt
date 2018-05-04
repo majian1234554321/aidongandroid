946093a86742e96735383a70543e992fd2b3aa8d
majian <99571202@qq.com>
2018-05-04 12:41:06 +0800

    活动详情页面转到H5

--- aidong/src/main/java/com/leyuan/aidong/ui/activities/interface/MyJSInterface.kt
+++ aidong/src/main/java/com/leyuan/aidong/ui/activities/interface/MyJSInterface.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import android.webkit.JavascriptInterface
 import com.facebook.stetho.common.LogUtil
 import com.leyuan.aidong.ui.App
+import com.leyuan.aidong.ui.home.activity.MapActivity
+import com.leyuan.aidong.ui.mine.activity.account.LoginActivity
 import com.leyuan.aidong.utils.DeviceManager
 import com.leyuan.aidong.utils.constant.PayType
 
@@ -16,7 +18,14 @@ class MyJSInterface(var mContext: Context?) {
     }
 
     fun showMap(lat: Double, lng: Double) {
+        if (App.getInstance().isLogin) {
 
+
+//        MapActivity.start(mContext, store.getName(), store.getName(), store.getAddress(),
+//                lat.toString(), lng.toString())
+        } else {
+
+        }
     }
 
 
