6773b139e22a3dc2f820a6f6f7a5e71965e0b959
majian <99571202@qq.com>
2018-06-13 18:17:38 +0800

    去除重复包名

--- aidong/src/main/java/com/example/aidong/ui/home/view/StoreHeaderView.java
+++ aidong/src/main/java/com/example/aidong/ui/home/view/StoreHeaderView.java
@@ -6,6 +6,7 @@ import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.GridView;
 import android.widget.ImageView;
@@ -13,16 +14,16 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.example.aidong.R;
-import com.example.aidong .entity.BannerBean;
+import com.example.aidong.entity.BannerBean;
 import com.example.aidong.entity.MarketPartsBean;
 import com.example.aidong.entity.SystemBean;
-import com.example.aidong .ui.MainActivity;
-import com.example.aidong .ui.home.activity.EquipmentActivity;
-import com.example.aidong .ui.home.activity.FoodAndBeverageActivity;
-import com.example.aidong .ui.home.activity.GoodsBrandRecommendActivity;
-import com.example.aidong .ui.home.activity.NurtureActivity;
+import com.example.aidong.ui.MainActivity;
+import com.example.aidong.ui.home.activity.EquipmentActivity;
+import com.example.aidong.ui.home.activity.FoodAndBeverageActivity;
+import com.example.aidong.ui.home.activity.GoodsBrandRecommendActivity;
+import com.example.aidong.ui.home.activity.NurtureActivity;
 import com.example.aidong.utils.Constant;
-import com.example.aidong .utils.GlideLoader;
+import com.example.aidong.utils.GlideLoader;
 import com.example.aidong.utils.SystemInfoUtils;
 
 import java.util.List;
@@ -33,14 +34,15 @@ import cn.bingoogolapple.bgabanner.BGABanner;
  * the header of store activity
  * Created by song on 2017/2/21.
  */
-public class StoreHeaderView extends RelativeLayout{
+public class StoreHeaderView extends RelativeLayout {
     private BGABanner banner;
+
     public StoreHeaderView(Context context) {
-        this(context,null,0);
+        this(context, null, 0);
     }
 
     public StoreHeaderView(Context context, AttributeSet attrs) {
-        this(context, attrs,0);
+        this(context, attrs, 0);
     }
 
     public StoreHeaderView(Context context, AttributeSet attrs, int defStyleAttr) {
@@ -48,76 +50,85 @@ public class StoreHeaderView extends RelativeLayout{
         initView(context);
     }
 
-    private void initView(final Context context){
-        View headerView = LayoutInflater.from(context).inflate(R.layout.header_store,this,true);
+    private void initView(final Context context) {
+        View headerView = LayoutInflater.from(context).inflate(R.layout.header_store, this, true);
         banner = (BGABanner) headerView.findViewById(R.id.banner);
 
 
         banner.setAdapter(new BGABanner.Adapter() {
             @Override
             public void fillBannerItem(BGABanner banner, View view, Object model, int position) {
-                GlideLoader.getInstance().displayImage(((BannerBean)model).getImage(), (ImageView)view);
+                GlideLoader.getInstance().displayImage(((BannerBean) model).getImage(), (ImageView) view);
             }
         });
 
         banner.setDelegate(new BGABanner.Delegate() {
             @Override
             public void onBannerItemClick(BGABanner banner, View itemView, Object model, int position) {
-                ((MainActivity)context).toTargetActivity((BannerBean)model);
+                ((MainActivity) context).toTargetActivity((BannerBean) model);
             }
         });
 
-        GridView gridView =  headerView.findViewById(R.id.gridview);
+        GridView gridView = headerView.findViewById(R.id.gridview);
 
-        MyGridAdapter gridAdapter =    new MyGridAdapter(context ,SystemInfoUtils.getMarketPartsBean(context));
+        MyGridAdapter gridAdapter = new MyGridAdapter(context, SystemInfoUtils.getMarketPartsBean(context));
 
         gridView.setAdapter(gridAdapter);
 
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(context, NurtureActivity.class);
+                intent.putExtra("position",position);
+                (context).startActivity(intent);
+            }
+        });
 
 
 //
-//        ll_nurture .setOnClickListener(new OnClickListener() {
+////        ll_nurture .setOnClickListener(new OnClickListener() {
+////            @Override
+////            public void onClick(View v) {
+////                Intent intent = new Intent(context, NurtureActivity.class);
+////                (context).startActivity(intent);
+////            }
+////        });
+//        headerView.findViewById(R.id.ll_equipment).setOnClickListener(new OnClickListener() {
 //            @Override
 //            public void onClick(View v) {
-//                Intent intent = new Intent(context, NurtureActivity.class);
+//                Intent intent = new Intent(context, EquipmentActivity.class);
 //                (context).startActivity(intent);
 //            }
 //        });
-        headerView.findViewById(R.id.ll_equipment).setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(context, EquipmentActivity.class);
-                (context).startActivity(intent);
-            }
-        });
-
-        headerView.findViewById(R.id.ll_healthy_food).setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(context, FoodAndBeverageActivity.class);
-                (context).startActivity(intent);
-            }
-        });
-        headerView.findViewById(R.id.ll_ticket).setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                GoodsBrandRecommendActivity.start(context, "ticket","票务赛事");
-            }
-        });
+//
+//        headerView.findViewById(R.id.ll_healthy_food).setOnClickListener(new OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(context, FoodAndBeverageActivity.class);
+//                (context).startActivity(intent);
+//            }
+//        });
+//        headerView.findViewById(R.id.ll_ticket).setOnClickListener(new OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                GoodsBrandRecommendActivity.start(context, "ticket", "票务赛事");
+//            }
+//        });
     }
 
-    public BGABanner getBannerView(){
+    public BGABanner getBannerView() {
         return banner;
     }
 
 
 
 
-    public class MyGridAdapter extends BaseAdapter{
+
+    public class MyGridAdapter extends BaseAdapter {
 
 
         public Context context;
-        public  List<MarketPartsBean> marketPartsBean;
+        public List<MarketPartsBean> marketPartsBean;
 
         public MyGridAdapter(Context context, List<MarketPartsBean> marketPartsBean) {
             this.context = context;
@@ -141,11 +152,14 @@ public class StoreHeaderView extends RelativeLayout{
 
         @Override
         public View getView(int i, View view, ViewGroup viewGroup) {
-            if (view!=null){
-                view =    View.inflate(context,R.layout.mygridadapter,null);
+            if (view != null) {
+                view = View.inflate(context, R.layout.mygridadapter, null);
             }
 
-            view.findViewById(R.id.)
+            ImageView iv = view.findViewById(R.id.iv);
+            TextView tv = view.findViewById(R.id.iv);
+            GlideLoader.getInstance().displayImage2(marketPartsBean.get(i).cover,iv);
+            tv.setText(marketPartsBean.get(i).name);
 
             return null;
         }
